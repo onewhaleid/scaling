1c3ca1849aa1f4e46f52f243425d2bd04120bbf5
Dan Howe <d.howe@wrl.unsw.edu.au>
2018-09-10 19:17:30 +1000

    Add tests

--- tests/froude_test.py
+++ tests/froude_test.py
@@ -4,8 +4,14 @@ from scaling import froude
 def test_unit_to_same_unit():
     assert froude.model_to_proto(1, 1, 'm', 'm') == 1
 
+def test_unit_to_different_unit():
+    assert froude.model_to_proto(1, 1, 'kN', 'N') == 1000
+
 def test_froude_length():
     assert froude.model_to_proto(1, 10, 'm', 'm') == 10
+
+def test_froude_time():
+    assert froude.model_to_proto(1, 25, 's', 's') == 5
 
 def test_froude_force():
     assert froude.model_to_proto(1, 10, 'N', 'N') == 1000
