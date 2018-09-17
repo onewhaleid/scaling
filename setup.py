d7924309c0f4264f0283ae0d7401e4f57fabc219
Dan Howe <d.howe@wrl.unsw.edu.au>
2018-09-18 07:33:11 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='scaling',
+    version='0.1.0',
+    packages=['scaling'],
+    install_requires=['pint'],
+    author='Dan Howe',
+    author_email='d.howe@wrl.unsw.edu.au',
+    description='convert units using Froude and Reynolds similitude')
