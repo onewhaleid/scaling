15f0cb2a627b309faedde31d0cc25b69186c210f
Dan Howe <d.howe@wrl.unsw.edu.au>
2018-09-18 17:17:11 +1000

    Prepare 0.2.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scaling',
-    version='0.1.0',
+    version='0.2.0',
     packages=['scaling'],
     install_requires=['pint'],
     author='Dan Howe',
