099e6d0f4768fe0cb95a9d41576559a621062bdf
Dan Howe <d.howe@wrl.unsw.edu.au>
2018-09-21 17:06:12 +1000

    Add project URL

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(
     install_requires=['pint'],
     author='Dan Howe',
     author_email='d.howe@wrl.unsw.edu.au',
+    url='https://github.com/onewhaleid/scaling'
     description='convert units using Froude and Reynolds similitude')
